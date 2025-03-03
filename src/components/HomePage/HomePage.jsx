39e3870abbc1ef031e83c2171d7a8c6554b8b112
huynqh0101 <huynqh0101@gmail.com>
2025-03-03 15:39:34 +0700

    updatecode 3/3

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,6 +1,9 @@
 import MyHeader from '@components/Header/Header';
 import styles from './styles.module.scss';
 import Banner from '@components/Banner/Banner';
+import AdvanceHeadling from '@components/AdvanceHeadling/AdvanceHeadling';
+import Info from '@components/Info/Info';
+import HeadingListProducts from '@components/HeadingListProduct/HeadingListProducts';
 function HomePage() {
     const { container } = styles;
     return (
@@ -8,6 +11,9 @@ function HomePage() {
             <div className={container}>
                 <MyHeader />
                 <Banner />
+                <Info />
+                <AdvanceHeadling />
+                <HeadingListProducts />
             </div>
         </div>
     );
