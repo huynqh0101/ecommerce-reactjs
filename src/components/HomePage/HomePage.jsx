c7831b4fe4593b20ae07982cd763934fe0d74701
huynqh0101 <huynqh0101@gmail.com>
2025-03-14 21:22:55 +0700

    14/3

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,21 +1,23 @@
 import MyHeader from '@components/Header/Header';
-import styles from './styles.module.scss';
 import Banner from '@components/Banner/Banner';
-import AdvanceHeadling from '@components/AdvanceHeadling/AdvanceHeadling';
 import Info from '@components/Info/Info';
+import AdvanceHeadling from '@components/AdvanceHeadling/AdvanceHeadling';
 import HeadingListProducts from '@components/HeadingListProduct/HeadingListProducts';
+
 function HomePage() {
-    const { container } = styles;
     return (
-        <div>
-            <div className={container}>
-                <MyHeader />
-                <Banner />
-                <Info />
-                <AdvanceHeadling />
-                <HeadingListProducts />
-            </div>
-        </div>
+        <>
+            <MyHeader />
+            <Banner />
+            <Info />
+            <AdvanceHeadling />
+            <HeadingListProducts />
+            <div
+                style={{
+                    height: '200px'
+                }}
+            ></div>
+        </>
     );
 }
 
