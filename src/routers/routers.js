c7a70e69fc802cc3d20c3b68afcdc5d5f6fca569
huynqh0101 <huynqh0101@gmail.com>
2025-03-18 22:10:05 +0700

    OurShop

--- src/routers/routers.js
+++ src/routers/routers.js
@@ -1,14 +1,18 @@
 import { lazy } from 'react';
- 
- const routers = [
-     {
-         path: '/',
-         component: lazy(() => import('@components/HomePage/HomePage'))
-     },
-     {
-         path: '/blog',
-         component: lazy(() => import('@components/Blog/Blog'))
-     }
- ];
- 
- export default routers;
+
+const routers = [
+    {
+        path: '/',
+        component: lazy(() => import('@components/HomePage/HomePage'))
+    },
+    {
+        path: '/blog',
+        component: lazy(() => import('@components/Blog/Blog'))
+    },
+    {
+        path: '/shop',
+        component: lazy(() => import('@pages/OurShop/OurShop'))
+    }
+];
+
+export default routers;
