f5b5ff958f88596bb8d95d10ba2cebe5b1b3923c
huynqh0101 <huynqh0101@gmail.com>
2025-03-20 00:05:20 +0700

    CartTable

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,22 +1,25 @@
 import MyFooter from '@components/Footer/Footer';
- import MyHeader from '@components/Header/Header';
- import Contents from '@/pages/Cart/components/contents/Contents';
- import Steps from '@/pages/Cart/components/steps/Steps';
- import styles from './styles.module.scss';
- 
- function Cart() {
-     const { container } = styles;
- 
-     return (
-         <>
-             <MyHeader />
-             <div className={container}>
-                 <Steps />
-                 <Contents />
-             </div>
-             <MyFooter />
-         </>
-     );
- }
- 
- export default Cart;
+import MyHeader from '@components/Header/Header';
+import Contents from '@/pages/Cart/components/contents/Contents';
+import Steps from '@/pages/Cart/components/steps/Steps';
+import styles from './styles.module.scss';
+import MainLayout from '@components/Layout/Layout';
+
+function Cart() {
+    const { container } = styles;
+
+    return (
+        <>
+            <MyHeader />
+            <div className={container}>
+                <Steps />
+                <MainLayout>
+                    <Contents />
+                </MainLayout>
+            </div>
+            <MyFooter />
+        </>
+    );
+}
+
+export default Cart;
