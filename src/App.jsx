d507179064492c33db122b7ace215f0030210f2b
huynqh0101 <huynqh0101@gmail.com>
2025-03-19 14:29:47 +0700

    feat: Add OurShopProvider & improve product display

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,29 @@ import { SidebarProvider } from '@/contexts/SideBarProvider';
 import SideBar from '@components/Sidebar/Sidebar';
 import { ToastProvider } from '@/contexts/ToastProvider';
 import { StoreProvider } from '@/contexts/storeProvider';
+import { OurShopProvider } from '@/contexts/OurShopProvider'; // ✅ Import OurShopProvider
 
 function App() {
     return (
         <StoreProvider>
             <ToastProvider>
                 <SidebarProvider>
-                    <SideBar />
-
-                    <BrowserRouter>
-                        <Suspense fallback={<div>Loading...</div>}>
-                            <Routes>
-                                {routers.map((item, index) => {
-                                    return (
+                    <OurShopProvider> {/* ✅ Bọc ứng dụng với OurShopProvider */}
+                        <SideBar />
+                        <BrowserRouter>
+                            <Suspense fallback={<div>Loading...</div>}>
+                                <Routes>
+                                    {routers.map((item, index) => (
                                         <Route
                                             path={item.path}
                                             element={<item.component />}
                                             key={index}
                                         />
-                                    );
-                                })}
-                            </Routes>
-                        </Suspense>
-                    </BrowserRouter>
+                                    ))}
+                                </Routes>
+                            </Suspense>
+                        </BrowserRouter>
+                    </OurShopProvider>
                 </SidebarProvider>
             </ToastProvider>
         </StoreProvider>
