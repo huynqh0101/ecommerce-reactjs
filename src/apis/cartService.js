8a3a6c0724eaf4eadebad663a854710841c252b6
huynqh0101 <huynqh0101@gmail.com>
2025-03-19 22:31:26 +0700

    Feat: Update Cart

--- src/apis/cartService.js
+++ src/apis/cartService.js
@@ -1,10 +1,17 @@
-import axiosClient from "./axiosClient";
+import axiosClient from './axiosClient';
 
 const addProductToCart = async (data) => {
     return await axiosClient.post('/cart', data);
 };
+
 const getCart = async (userId) => {
-    return await axiosClient.get('/cart/${userId}');
+    return await axiosClient.get(`/cart/${userId}`);
 };
-export { addProductToCart, getCart };
-// Compare this snippet from src/apis/productService.js:       
+
+const deleteItem = async (body) => {
+    return await axiosClient.delete(`/cart/deleteItem`, {
+        data: body
+    });
+};
+
+export { addProductToCart, getCart, deleteItem };
