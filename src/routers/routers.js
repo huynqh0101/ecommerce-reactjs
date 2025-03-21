bb72d7e0e607801a33680ff08d903841c9c1df2e
huynqh0101 <huynqh0101@gmail.com>
2025-03-21 18:11:02 +0700

    update detail Product

--- src/routers/routers.js
+++ src/routers/routers.js
@@ -13,9 +13,13 @@ const routers = [
         path: '/shop',
         component: lazy(() => import('@pages/OurShop/OurShop'))
     },
-    { 
+    {
         path: '/cart',
-        component: lazy(() => import('@pages/Cart/Cart'))       
+        component: lazy(() => import('@pages/Cart/Cart'))
+    },
+    {
+        path: '/product/:id',
+        component: lazy(() => import('@pages/DetailProduct'))
     }
 ];
 
